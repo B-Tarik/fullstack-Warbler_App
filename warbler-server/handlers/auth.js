c366721a46544b7ec5b227558a76fe208a9a4943
B-Tarik <tarik.bouchekkif@gmail.com>
2019-01-29 17:37:44 +0000

    finished warbler-server

---
+++ warbler-server/handlers/auth.js
@@ -0,0 +1,62 @@
+const db = require('../models');
+const jwt = require('jsonwebtoken');
+
+exports.signin = async function(req, res, next) {
+  try {
+    const user = await db.User.findOne({
+      email: req.body.email
+    });
+    const {id, username, profileImageUrl} = user;
+    const isMatch = await user.comparePassword(req.body.password);
+    if(isMatch) {
+      const token = jwt.sign({
+        id,
+        username,
+        profileImageUrl
+      }, process.env.SECRET_KEY);
+      return res.status(200).json({
+        id,
+        username,
+        profileImageUrl,
+        token
+      });
+    } else {
+      return next({
+        status: 400,
+        message: 'Invalid Email/Password.'
+      })
+    }
+  } catch (error) {
+    return next({
+      status: 400,
+      message: 'Invalid Email/Password.'
+    })
+  }
+}
+
+exports.signup = async function(req, res, next) {
+  try {
+    const user = await db.User.create(req.body);
+    const {id, username, profileImageUrl} = user;
+    const token = jwt.sign({
+      id,
+      username,
+      profileImageUrl
+    }, process.env.SECRET_KEY);
+    return res.status(200).json({
+      id,
+      username,
+      profileImageUrl,
+      token
+    });
+  } catch(err) {
+    // if validation fails!
+    if(err.code === 11000) {
+      err.message = 'Sorry, that username and/or email is taken';
+    }
+    return next({
+      status: 400,
+      message: err.message
+    })
+  }
+}
